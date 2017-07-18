8dcee23fccfe63844e9baeab8806757fac61f101
Jiahao Li <isundaylee.reg@gmail.com>
2017-07-18 01:19:27 -0700

    Rubocop comes to inspect stuff

--- app.rb
+++ app.rb
@@ -6,16 +6,16 @@ require_relative 'digital_ocean_ddns_client'
 
 class IPApp < Sinatra::Base
   get '/' do
-    "#{request.ip}"
+    request.ip.to_s
   end
 
   post '/ddns/:name' do
     name = params[:name]
-    access_token = ENV["DIGITAL_OCEAN_TOKEN"]
-    domain_name = ENV["DOMAIN_NAME"]
+    access_token = ENV['DIGITAL_OCEAN_TOKEN']
+    domain_name = ENV['DOMAIN_NAME']
     ip = request.ip
 
-    return "Authentication failed." unless ENV["SECRET_KEY"] == params[:secret_key]
+    return 'Authentication failed.' unless ENV['SECRET_KEY'] == params[:secret_key]
 
     begin
       client = DigitalOceanDDNSClient.new(access_token, domain_name)
@@ -27,5 +27,5 @@ class IPApp < Sinatra::Base
     return "Successfully set #{name}.#{domain_name} to point to #{ip}."
   end
 
-  run! if app_file == 0
+  run! if app_file.zero?
 end
--- digital_ocean_ddns_client.rb
+++ digital_ocean_ddns_client.rb
@@ -3,40 +3,49 @@ require 'droplet_kit'
 class DigitalOceanDDNSException < RuntimeError; end
 
 class DigitalOceanDDNSClient
-
   TOKEN_REGEX = /[a-z0-9]{64}/
   DOMAIN_NAME_REGEX = /[a-z0-9A-Z]+\.[a-z0-9A-Z]+/
 
   def initialize(token, domain_name)
-    raise DigitalOceanDDNSException("Invalid token.") unless token =~ TOKEN_REGEX
-    raise DigitalOceanDDNSException("Invalid domain name.") unless domain_name =~ DOMAIN_NAME_REGEX
+    raise DigitalOceanDDNSException('Invalid token.') unless token =~ TOKEN_REGEX
+    raise DigitalOceanDDNSException('Invalid domain name.') unless domain_name =~ DOMAIN_NAME_REGEX
 
     @domain_name = domain_name
     @client = DropletKit::Client.new(access_token: token)
   end
 
   def set(name, ip)
-    records = @client.domain_records.all(for_domain: @domain_name)
-    matches = records.select { |r| r.name == name && r.type == 'A' }
-
-    raise DigitalOceanDDNSException("More than 1 existing records found.") if matches.size > 1
+    match = find_existing_record(name)
+    record = generate_new_record(name, ip)
 
     begin
-      record = DropletKit::DomainRecord.new(
-        type: 'A',
-        name: name,
-        data: ip
-      )
-
-      if matches.empty?
+      if match.nil?
         @client.domain_records.create(record, for_domain: @domain_name)
       else
         @client.domain_records.update(record, for_domain: @domain_name,
-                                              id: matches.first.id)
+                                              id: match.id)
       end
     rescue DropletKit::Error => e
-      raise DigitalOceanDDNSException.new(e.message)
+      raise DigitalOceanDDNSException, e.message
     end
   end
 
+  private
+
+  def find_existing_record(name)
+    records = @client.domain_records.all(for_domain: @domain_name)
+    matches = records.select { |r| r.name == name && r.type == 'A' }
+
+    raise DigitalOceanDDNSException('More than 1 existing records found.') if matches.size > 1
+
+    matches.first
+  end
+
+  def generate_new_record(name, ip)
+    DropletKit::DomainRecord.new(
+      type: 'A',
+      name: name,
+      data: ip
+    )
+  end
 end
